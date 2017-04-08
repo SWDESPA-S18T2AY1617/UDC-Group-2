66d1f0efd392df24b7051333aba975b57f74d496
louisecortez <louise_cortez@dlsu.edu.ph>
2017-04-08 13:14:34 +0800

    Added login() method

--- src/model/calendar/Secretary.java
+++ src/model/calendar/Secretary.java
@@ -1,10 +1,19 @@
 package model.calendar;
 
+import model.storage.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Secretary {
 	private String username;
 	private String password;
 	private Name name;
 	private int id;
+	
+	private ArrayList<Doctor> doctors;
+	private AppointmentCollection appointmentCollection;
+	private ArrayList<model.calendar.Appointment> appointments; // appointments set by the secretary
+	
 	
 	public static final String TABLE = "secretary";
 	public static final String COL_ID = "sec_id";
@@ -13,6 +22,18 @@ public class Secretary {
 	public static final String COL_MIDDLENAME = "middle_name";
 	public static final String COL_LASTNAME = "last_name";
 	public static final String COL_USERNAME = "username";
+	
+	public Secretary() {
+		doctors = new ArrayList<Doctor>();
+		appointments = new ArrayList<model.calendar.Appointment>();
+	}
+	
+	public boolean login(String username, String password) {
+		if(this.getUsername().equals(username) && this.getPassword().equals(password))
+			return true;
+		else 
+			return false;
+	}
 	
 	public Name getName() {
 		return name;
@@ -45,4 +66,37 @@ public class Secretary {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public void setAppCollection(AppointmentCollection appointmentCollection) {
+		this.appointmentCollection = appointmentCollection; 
+	}
+	
+	public void setDoctors(ArrayList<Doctor> doc) {
+		Iterator<Doctor> d = doc.iterator();
+		
+		while(d.hasNext()) {
+			Doctor doctor = d.next();
+			doctors.add(doctor);
+		}
+	}
+	
+	public void addDoctor(Doctor d) {
+		if(d != null)
+			doctors.add(d);
+	}
+	
+	public boolean setAppointment(model.calendar.Appointment app) {
+		appointments.add(app);
+		return appointmentCollection.update(app);
+	}
+	
+	public boolean cancelAppointment(model.calendar.Appointment app) {
+		if(appointments.contains(app)) {
+			appointments.remove(app);
+			return appointmentCollection.delete(app);
+		}
+		
+		return false;	
+	}
+	
 }
