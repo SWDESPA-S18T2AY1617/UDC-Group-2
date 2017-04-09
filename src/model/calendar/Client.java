1540d1f5d3a58335704a28cb3b16affaa51c0bff
Sophia Therese T. Rivera <sophia_rivera@dlsu.edu.ph>
2017-04-09 11:14:30 +0800

    Added login() method, add & cancel app

--- src/model/calendar/Client.java
+++ src/model/calendar/Client.java
@@ -1,14 +1,52 @@
 package model.calendar;
 
+import java.util.ArrayList;
+
+import model.storage.AppointmentCollection;
+
 public class Client {
 	private int id;
 	private Name name;
+	
+	private String username;	//for log-in
+	private String password;
+	
+	private AppointmentCollection appointmentCollection;
+	private ArrayList<model.calendar.Appointment> appointments; // appointments set by client
+	
 	
 	public static final String TABLE = "client";
 	public static final String COL_ID = "client_id";
 	public static final String COL_FIRSTNAME = "first_name";
 	public static final String COL_MIDDLENAME = "middle_name";
 	public static final String COL_LASTNAME = "last_name";
+	
+	public Client() {
+		appointments = new ArrayList<model.calendar.Appointment>();
+	}
+	
+	public boolean login(String username, String password) {
+		if(this.getUsername().equals(username) && this.getPassword().equals(password))
+			return true;
+		else 
+			return false;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 	public Name getName() {
 		return name;
@@ -24,5 +62,23 @@ public class Client {
 	
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	public void setAppCollection(AppointmentCollection appointmentCollection) {
+		this.appointmentCollection = appointmentCollection; 
+	}
+	
+	public boolean setAppointment(model.calendar.Appointment app) {
+		appointments.add(app);
+		return appointmentCollection.update(app);
+	}
+	
+	public boolean cancelAppointment(model.calendar.Appointment app) {
+		if(appointments.contains(app)) {
+			appointments.remove(app);
+			return appointmentCollection.delete(app);
+		}
+		
+		return false;	
 	}
 }
